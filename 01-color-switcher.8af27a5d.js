b806166959ed8eb970c7cc7a6aecde4a1e1afed3
AndreeaBartic <andreeabartic20@gmail.com>
2023-10-12 12:37:53 +0300

    dist

---
+++ 01-color-switcher.8af27a5d.js
@@ -0,0 +1,2 @@
+let t;document.querySelector("[data-start]").addEventListener("click",(function(){this.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),document.querySelector("[data-stop]").addEventListener("click",(function(){clearInterval(t),document.querySelector("[data-start]").disabled=!1}));
+//# sourceMappingURL=01-color-switcher.8af27a5d.js.map
